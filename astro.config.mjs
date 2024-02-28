9fca0f15c306cc1e413aa820c8c49aef2a187bf3
ImJonahCodes <stjhoffman6@students.pghschools.org>
2024-02-27 22:28:42 -0500

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), robotsTxt()],
-  site: 'https://simple-astro-blog.vercel.app',cl
+  site: 'https://simple-astro-blog.vercel.app',
   output: 'server',
   adapter: vercel(),
 })

01e4ecbfe002635bf1aefdc1ee47a4d8ae97b890
ImJonahCodes <stjhoffman6@students.pghschools.org>
2024-02-27 22:23:33 -0500

    updated modules

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import robotsTxt from 'astro-robots-txt'
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), robotsTxt()],
-  site: 'https://simple-astro-blog.vercel.app'
+  site: 'https://simple-astro-blog.vercel.app',cl
+  output: 'server',
+  adapter: vercel(),
 })
